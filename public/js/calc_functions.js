ad1c2ca879cbedebe6b82059108e0ab324edbe50
AdrielDavi <104457083+AdrielDavi@users.noreply.github.com>
2022-05-10 18:04:28 -0300

    Update calc_functions.js

--- public/js/calc_functions.js
+++ public/js/calc_functions.js
@@ -1,32 +1,48 @@
+var valor1 = ""
+var mostrar = ""
 function atualizarDisplay(btn){
     const display = document.getElementById('display');
-    if(display.value.length === 9) return;
+    valor1 = valor1 + btn.value;
+    
     if(display.value === '0') display.value = btn.value;
     else display.value += btn.value;
+    
+    
 }
 
 function limparDisplay(){
     document.getElementById('display').value = '0';
+    valor1 = ''
 }
 
 var operador = ''
-var valor1 = 0
 function atualizarOperacao(btn){
     const display = document.getElementById('display');
-    operador = btn.value;
-    valor1 = parseInt(display.value);
-    display.value = '0';
+    valor1 = btn.value;
+    valor1 = parseFloat(display.value);
+    
 }
 
 function calcularOperacao(){
     const display = document.getElementById('display');
-    const valor2 = parseInt(display.value);
-    valor1 = eval(valor1+operador+valor2);
-    display.value = valor1;
-    operador = '';
+    mostrar = valor1
+    display.value = eval(mostrar);
+    valor1= ""
+    
+   
 }       
 
 function manipularTeclado(){
     if(/[0-9]/.test(event.key))
         atualizarDisplay({value: event.key});
+}
+function raiz(){
+    const display = document.getElementById('display');
+    valor1 = parseFloat(display.value);
+    display.value = Math.sqrt(valor1);
+}
+function expoente(){
+    const display = document.getElementById('display');
+    valor1 = parseFloat(display.value);
+    display.value = valor1**2;
 }
